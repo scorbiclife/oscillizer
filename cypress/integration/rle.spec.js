93149a29592ca1d7c76e842c24a2acb198d52325
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-07-14 22:41:10 +0900

    Add: RLE parsing code

---
+++ cypress/integration/rle.spec.js
@@ -0,0 +1,70 @@
+import * as rle from '../../docs/lib/rle';
+
+describe('RLE body extractor', () => {
+  it('Should process a simple RLE correctly', () => {
+    const rleString = (
+      `x = 3, y = 3, rule = B3/S23
+        bo$
+        2bo$
+        3o!
+      `
+    );
+    expect(rle.extractBody(rleString)).to.equal('bo$2bo$3o!');
+  });
+
+  it('Should process RLE with comments correctly', () => {
+    const rleString = (
+      `# Some comment line
+      # Another comment line
+      x = 3, y = 3, rule = B3/S23
+          bo$
+          2bo$
+          3o!
+      `
+    );
+    expect(rle.extractBody(rleString)).to.equal('bo$2bo$3o!');
+  });
+
+  it('Should process RLE without headers correctly', () => {
+    const rleString = (
+      `
+        bo$
+        2bo$
+        3o!
+      `
+    );
+    expect(rle.extractBody(rleString)).to.equal('bo$2bo$3o!');
+  });
+});
+
+describe('RLE Parser state', () => {
+  it('Should process newlines correctly', () => {
+    const state = new rle.ParserState({ runCount: 10, currentCell: [0, 0] });
+    state.addNewlines();
+    expect(state.runCount).to.equal(0);
+    expect(state.currentCell).to.deep.equal([0, 10]);
+  });
+
+  it('Should update run counts correctly', () => {
+    const state = new rle.ParserState({ runCount: 10, currentCell: [1, 2] });
+    state.updateRunCount(3);
+    expect(state.runCount).to.equal(103);
+    expect(state.currentCell).to.deep.equal([1, 2]);
+  });
+
+  it('Should process new cells correctly', () => {
+    const state = new rle.ParserState(
+      { cells: [], runCount: 3, currentCell: [1, 2] }
+    );
+    state.drawRun(1);
+    expect(state.runCount).to.equal(0);
+    expect(state.cells).to.deep.equal([[1, 2], [2, 2], [3, 2]]);
+  });
+});
+
+describe('RLE body parser', () => {
+  it('Should parse the glider correctly', () => {
+    const gliderCells = [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]];
+    expect(rle.parseBody('bo$2bo$3o!')).to.have.deep.members(gliderCells);
+  });
+});
---
+++ docs/lib/rle.js
@@ -0,0 +1,107 @@
+/* Body extracting related code */
+
+export const extractBody = (rleString) => {
+  const lines = (
+    rleString.split('\n')
+      .filter((line) => line.length > 0)
+      .map((line) => line.replace(/\s/g, ''))
+  );
+
+  const isNotComment = (line) => !line.startsWith('#');
+  const firstNonCommentIndex = lines.findIndex(isNotComment);
+
+  const headerRegex = /^x=\d+,y=\d+,rule=/;
+  const isHeader = (line) => headerRegex.test(line);
+  const bodyStartIndex = (
+    firstNonCommentIndex + (isHeader(lines[firstNonCommentIndex]) ? 1 : 0)
+  );
+  return lines.slice(bodyStartIndex).join('');
+};
+
+/* Parsing related code */
+
+export class ParserState {
+  constructor(source = {}) {
+    this.isFinished = source.isFinished || false;
+    this.cells = source.cells || [];
+    this.runCount = source.runCount || 0;
+    this.currentCell = source.currentCell || [0, 0];
+  }
+
+  finishParsing() {
+    this.isFinished = true;
+  }
+
+  addNewlines() {
+    const [/* x */, y] = this.currentCell;
+    const runCount = this.runCount || 1;
+    this.currentCell = [0, y + runCount];
+    this.runCount = 0;
+  }
+
+  // Append digit: integer to runCount: integer
+  updateRunCount(digit) {
+    this.runCount = this.runCount * 10 + digit;
+  }
+
+  drawRun(state) {
+    const [x, y] = this.currentCell;
+    const runCount = this.runCount || 1;
+    const newCells = new Array(runCount).fill(0).map((v, i) => [x + i, y]);
+
+    // Refactor addNewCells into a dependency if needs variation in behavior
+    const addNewCells = (nc, s) => {
+      if (s) { this.cells = this.cells.concat(nc); }
+    };
+    addNewCells(newCells, state);
+
+    this.currentCell = [x + runCount, y];
+    this.runCount = 0;
+  }
+}
+
+const mapStateFromChar = new Map(
+  [
+    ['.', 0], ['A', 1], ['B', 0], ['C', 1], ['D', 0], ['E', 1], ['F', 0],
+    ['b', 0], ['o', 1],
+  ]
+);
+
+const updateParserState = (parserState, c) => {
+  // Passthrough if the parser is finished.
+  if (parserState.isFinished) {
+    return parserState;
+  }
+
+  if (c === '!') {
+    parserState.finishParsing();
+    return parserState;
+  }
+
+  // Update the run count if it is a digit
+  if (c >= '0' && c <= '9') {
+    const d = c.charCodeAt(0) - '0'.charCodeAt(0);
+    parserState.updateRunCount(d);
+    return parserState;
+  }
+
+  // Jump to next line if it is '$'
+  if (c === '$') {
+    parserState.addNewlines();
+    return parserState;
+  }
+
+  // Draw the run if it is a character
+  const currState = mapStateFromChar.get(c) || 0;
+  parserState.drawRun(currState);
+  return parserState;
+};
+
+// Convert RLE without headers into a pattern of form [[x, y]]
+export const parseBody = (rleBodyString) => {
+  const parseResult = (
+    [...rleBodyString].reduce(updateParserState, new ParserState())
+  );
+  parseResult.finishParsing(); // No-op for now but semantically needed
+  return parseResult.cells;
+};
