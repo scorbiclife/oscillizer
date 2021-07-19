19b45a915813aa294dbab83dc3148f1e0e4fcdbc
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-07-19 09:03:13 +0900

    Add: osc.getBoundingBox

--- docs/lib/ca/osc.js
+++ docs/lib/ca/osc.js
@@ -77,3 +77,18 @@ export const getSubperiods = (pattern, maxPeriod = 1000) => {
   });
   return patternSubperiods;
 };
+
+export const getBoundingBox = (cells) => {
+  const addCellToBoundingBox = (box, [x, y]) => ({
+    xmin: (x < box.xmin) ? x : box.xmin,
+    xmax: (x > box.xmax) ? x : box.xmax,
+    ymin: (y < box.ymin) ? y : box.ymin,
+    ymax: (y > box.ymax) ? y : box.ymax,
+  });
+
+  const initialBox = {
+    xmin: Infinity, ymin: Infinity, xmax: -Infinity, ymax: -Infinity,
+  };
+
+  return cells.reduce(addCellToBoundingBox, initialBox);
+};
