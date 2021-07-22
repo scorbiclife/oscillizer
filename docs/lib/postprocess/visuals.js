a7181a0fb70e75c5855ac7d3aaea360163ab7a5b
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-07-22 23:04:46 +0900

    Change: Always use HSL colors than random colors
    Before the page generated random colors
    when there were more than 16 colors.
    
    However the colors generated are generally worse
    especially compared to stators and alive cells.
    
    It is now changed to always use HSL colors.

--- docs/lib/postprocess/visuals.js
+++ docs/lib/postprocess/visuals.js
@@ -4,15 +4,7 @@ export const colorscheme = {
   strictRotor: '#999999',
 };
 
-const makeRandomColor = () => {
-  const makeRandomUInt8 = () => Math.floor(Math.random() * 256);
-  const r = makeRandomUInt8();
-  const g = makeRandomUInt8();
-  const b = makeRandomUInt8();
-  return `rgb(${r},${g},${b})`;
-};
-
-const makeGradientColor = (numColors) => (i) => {
+const makeGradientColor = (numColors, i) => {
   const hue = Math.floor(360 * (i / numColors));
   return `hsl(${hue}, 100%, 70%)`;
 };
@@ -21,13 +13,8 @@ export const makeColorMap = (period, subperiods) => {
   const sortedSubperiods = subperiods.slice().sort((a, b) => a - b);
   const rotorSubperiods = sortedSubperiods.slice(1);
 
-  const indexToColor = (
-    (rotorSubperiods.length > 16)
-      ? makeRandomColor
-      : makeGradientColor(rotorSubperiods.length)
-  );
   const rotorSubperiodsAndColors = rotorSubperiods.map(
-    (sp, i) => [sp, indexToColor(i)]
+    (sp, i) => [sp, makeGradientColor(rotorSubperiods.length, i)]
   );
 
   return new Map([
