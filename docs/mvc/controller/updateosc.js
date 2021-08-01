910948c3a5851bb98091196a6a869565e069c252
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-08-02 00:48:08 +0900

    Refactor: Split osc info / osc stats logically
    The function is one big function due to speed issues,
    but at least they are split into logical units.

--- docs/mvc/controller/updateosc.js
+++ docs/mvc/controller/updateosc.js
@@ -27,62 +27,97 @@ const getStrictRotorCount = (subperiods, period) => (
 );
 
 const makeUpdateOscInfoAndStats = (appState, source) => (event) => {
+  const getOscData = (pattern) => {
+    const phases = osc.getAllPhases(pattern);
+    const period = phases.length;
+    if (period === 0) {
+      return { success: false };
+    }
+    const subperiodsByCell = osc.getSubperiodByCell(phases);
+    return {
+      success: true,
+      pattern,
+      phases,
+      subperiods: subperiodsByCell,
+    };
+  };
+
+  const getOscInfo = (oscData) => {
+    const {
+      success, pattern, phases, subperiods,
+    } = oscData;
+
+    if (!success) {
+      return { success: false };
+    }
+
+    const boundingBox = getBoundingBox(subperiods.map(({ cell }) => cell));
+
+    return {
+      success: true,
+      pattern,
+      period: phases.length,
+      subperiods,
+      boundingBox,
+    };
+  };
+
+  const getOscStats = (oscData) => {
+    const {
+      success, phases, subperiods,
+    } = oscData;
+
+    if (!success) {
+      return { success: false };
+    }
+
+    const period = phases.length;
+
+    const populations = getPopulations(phases);
+    const minPop = Math.min(...populations);
+    const maxPop = Math.max(...populations);
+    const avgPop = (
+      populations.reduce((a, b) => a + b, 0) / populations.length
+    ).toFixed(2);
+
+    const numRotorCells = getRotorCount(subperiods);
+    const numCells = subperiods.length;
+    const numStatorCells = numCells - numRotorCells;
+    const numStrictRotorCells = getStrictRotorCount(subperiods, period);
+    const volatility = `${((numRotorCells / numCells) * 100).toFixed(2)}%`;
+    const strictVolatility = `${((numStrictRotorCells / numCells) * 100).toFixed(2)}%`;
+
+    const boundingBox = getBoundingBox(subperiods.map(({ cell }) => cell));
+    const width = boundingBox.xmax - boundingBox.xmin + 1;
+    const height = boundingBox.ymax - boundingBox.ymin + 1;
+
+    const result = {
+      success: true,
+      period,
+      minPop,
+      maxPop,
+      avgPop,
+      numRotorCells,
+      numStatorCells,
+      numStrictRotorCells,
+      numCells,
+      volatility,
+      strictVolatility,
+      width,
+      height,
+    };
+    return result;
+  };
+
+  // Main function logic
   if (!event.target) {
     return;
   }
 
   const pattern = rle.parse(source.value);
-  const phases = osc.getAllPhases(pattern);
-  const period = phases.length;
-  if (period === 0) {
-    appState.oscInfo.setValue({ success: false });
-    appState.oscStatistics.setValue({ success: false });
-    return;
-  }
-  const subperiodByCell = osc.getSubperiodByCell(phases);
-  const boundingBox = getBoundingBox(subperiodByCell.map(({ cell }) => cell));
-
-  appState.oscInfo.setValue({
-    success: true,
-    pattern,
-    period,
-    subperiods: subperiodByCell,
-    boundingBox,
-  });
-
-  // Population statistics
-  const populations = getPopulations(phases);
-  const minPop = Math.min(...populations);
-  const maxPop = Math.max(...populations);
-  const avgPop = (
-    populations.reduce((a, b) => a + b, 0) / populations.length
-  ).toFixed(2);
-
-  const numRotorCells = getRotorCount(subperiodByCell);
-  const numCells = subperiodByCell.length;
-  const numStatorCells = numCells - numRotorCells;
-  const numStrictRotorCells = getStrictRotorCount(subperiodByCell, period);
-  const volatility = `${((numRotorCells / numCells) * 100).toFixed(2)}%`;
-  const strictVolatility = `${((numStrictRotorCells / numCells) * 100).toFixed(2)}%`;
-
-  const width = boundingBox.xmax - boundingBox.xmin + 1;
-  const height = boundingBox.ymax - boundingBox.ymin + 1;
-
-  appState.oscStatistics.setValue({
-    success: true,
-    period,
-    minPop,
-    maxPop,
-    avgPop,
-    numRotorCells,
-    numStatorCells,
-    numStrictRotorCells,
-    numCells,
-    volatility,
-    strictVolatility,
-    width,
-    height,
-  });
+  const oscData = getOscData(pattern);
+  appState.oscInfo.setValue(getOscInfo(oscData));
+  appState.oscStatistics.setValue(getOscStats(oscData));
 };
 
 export default makeUpdateOscInfoAndStats;
