6e7050de626f3eeab95492e89b57bf73a80982f2
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-08-09 17:10:48 +0900

    Refactor: Oscillator stats to use IView

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,7 @@ import AppState from './MVC/Models/AppState.js';
 import EventTargetPassingController from './MVC/Controllers/PassingController/EventTargetPassingController.js';
 import OscStatsController from './MVC/Controllers/OscStatsController/OscStatsController.js';
 import OscillizerCanvasView from './MVC/Views/OscillizerCanvasView/OscillizerCanvasView.js';
-import makeUpdateOscillizerStatistics from './MVC/Views/updateoscillizerstatistics.js';
+import OscStatsView from './MVC/Views/OscStatsView/OscStatsView.js';
 
 const appState = {
   oscInfo: new AppState(),
@@ -41,5 +41,5 @@ const oscCanvasView = new OscillizerCanvasView(
 appState.oscInfo.eventTarget.addEventListener('change', oscCanvasView.update);
 
 const oscStatsElement = document.getElementById('output-osc-data');
-const updateStats = makeUpdateOscillizerStatistics(appState.oscInfo, oscStatsElement);
-appState.oscInfo.eventTarget.addEventListener('change', updateStats);
+const oscStatsView = new OscStatsView(appState.oscInfo, oscStatsElement);
+appState.oscInfo.eventTarget.addEventListener('change', oscStatsView.update);
