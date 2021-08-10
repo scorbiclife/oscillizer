9f0ec47a7b61d72c04a5af29f5d8a10c0ca28fa6
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-08-10 15:48:58 +0900

    Add: JSDoc annotations for AppState

--- docs/MVC/Models/AppState.js
+++ docs/MVC/Models/AppState.js
@@ -1,14 +1,29 @@
-export default class AppState {
+/**
+ * A class that represents a single group of state.
+ * @class
+ */
+class AppState {
+  /**
+   * @param {*} value - The initial value of this state
+   * @param {EventTarget} [eventTarget] - The EventTarget to use for firing state changes
+   */
   constructor(value, eventTarget) {
+    /** @type {EventTarget} */
     this.eventTarget = eventTarget || new EventTarget();
+    /** @type {*} */
     this.value = value;
   }
 
-  // Set value and trigger onChange event
+  /**
+   * Set the value and fire `change` event.
+   * @param {*} newValue - The new value
+   */
   setValue(newValue) {
     this.value = newValue;
     this.eventTarget.dispatchEvent(
-      new CustomEvent('change', { detail: this.value })
+      new CustomEvent('change', { detail: { source: this } })
     );
   }
 }
+
+export default AppState;
