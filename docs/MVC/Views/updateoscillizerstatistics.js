7a6873d65ab01343e06f27b4f487a89d495209b5
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-08-10 15:45:54 +0900

    Refactor: Delete refactoring artifacts

--- docs/MVC/Views/updateoscillizerstatistics.js
+++
@@ -1,6 +0,0 @@
-const makeUpdateOscillatorStatistics = (source, targetElement) => (/* event */) => {
-  /* eslint-disable no-param-reassign */
-  targetElement.textContent = JSON.stringify(source, undefined, 2);
-};
-
-export default makeUpdateOscillatorStatistics;
