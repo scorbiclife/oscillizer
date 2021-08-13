4c0c1f2b2fe1f92efb5d681d6b4edbb26a58167d
nightlyherb <82109216+nightlyherb@users.noreply.github.com>
2021-08-14 00:40:58 +0900

    Refactor: Remove duplicate files

--- docs/Engine/BaseTypes/Cell.js
+++
@@ -1,7 +0,0 @@
-/**
- * The `Cell` datatype that represents a single cell.
- * @typedef Cell
- * @type {Array}
- * @property {number} 0 - The x coordinate
- * @property {number} 1 - The y coordinate
- */
--- docs/Engine/BaseTypes/MultiStatePattern.js
+++
@@ -1,9 +0,0 @@
-/**
- * The `MultiStatePattern` datatype that represents multiple cells,
- * with their respective states.
- * This is used for exchaging multi-state pattern data throughout the codebase.
- * For rationale, see {@link TwoStatePattern}.
- *
- * @typedef MultiStatePattern
- * @type {Array<CellEntry>}
- */
