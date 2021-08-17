9826af0e8f6c27bd498d2e7f1e42abe1240f96ba
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-08-17 13:59:36 +0900

    Add: Type annotations for IView

--- docs/MVC/Views/IView.js
+++ docs/MVC/Views/IView.js
@@ -1,7 +1,7 @@
 /**
  * A common interface that defines a view.
- * @interface IView
- * @property {function(Event)} update
+ * @typedef {Object} IView
+ * @property {function(Event): void} update
  *  A callback to update the view.
  *  The same precautions in {@link IController} apply.
  */
