a201e58dfa79ff3422364f3127acc1ce63eebafd
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-08-17 13:54:53 +0900

    Add: Type annotations for Osc controller

--- docs/MVC/Controllers/OscStatsController/OscStatsController.js
+++ docs/MVC/Controllers/OscStatsController/OscStatsController.js
@@ -3,21 +3,34 @@ import { getOscStats } from './OscHelpers.js';
 import * as AppConfig from '../../../AppConfig.js';
 
 /**
+ * @typedef {import('../IController').IController}  IController
+ * @typedef {import('../../Models/AppState').default} AppState
+ */
+
+/**
  * A controller that updates the target with osc stats, with the given RLE.
  * @class
  * @implements {IController}
  */
 class OscStatsController {
+  /**
+   * @param {AppState} targetState
+   * @param {HTMLElement} sourceElement
+   */
   constructor(targetState, sourceElement) {
     /** @type {AppState} */
     this.targetState = targetState;
     /** @type {HTMLElement} */
     this.sourceElement = sourceElement;
-    /** The update callback.  */
+    /**
+     * The update callback.
+     * @type {function(Event): void}
+     */
     this.update = (event) => {
       if (!event.target) {
         return;
       }
+      // @ts-ignore
       const { pattern, rule } = parseRLE(this.sourceElement.value);
       const initialBoard = AppConfig.makeBoard(pattern, rule);
       this.targetState.setValue(getOscStats(initialBoard));
