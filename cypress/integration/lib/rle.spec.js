0dd1175fb7e78f2d4a7cad0096a851f3f550a81c
nightlyherb <82109216+nightlyherb@users.noreply.github.com>
2023-06-20 11:58:20 +0900

    Setup Parcel (#29)
    * ops: edit gitignore
    
    Added Github gitignore tempalate.
    Also chose to commit package-lock.json for reproducibility
    
    * ops: remove jsdoc from README
    
    drop jsdoc support
    
    * ops: setup parcel

--- cypress/integration/lib/rle.spec.js
+++ cypress/integration/lib/rle.spec.js
@@ -1,8 +1,14 @@
-import * as rle from '../../../docs/MVC/Controllers/OscStatsController/RLEHelpers.js';
+import * as rle from "../../../src/MVC/Controllers/OscStatsController/RLEHelpers.js";
 
-describe('RLE body parser', () => {
-  it('should parse the glider correctly', () => {
-    const gliderCells = [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]];
-    expect(rle.parseBody('bo$2bo$3o!')).to.have.deep.members(gliderCells);
+describe("RLE body parser", () => {
+  it("should parse the glider correctly", () => {
+    const gliderCells = [
+      [1, 0],
+      [2, 1],
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ];
+    expect(rle.parseBody("bo$2bo$3o!")).to.have.deep.members(gliderCells);
   });
 });
